deaa7f94389f62273e3fe157b0e878ed72f9a827
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-10-09 17:53:40 +0900

    Initial Commit

---
+++ location-estimator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  location-estimator
+//
+//  Created by 冨田 直希 on 2018/10/09.
+//  Copyright © 2018年 冨田 直希. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
